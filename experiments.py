6a0fd80a4da7c505f9e0b68f50f720e6d1f6d69d
rblinde <robertblinde@gmail.com>
2020-01-25 17:11:31 +0100

    updated experiments

--- experiments.py
+++ experiments.py
@@ -17,30 +17,40 @@ def write_to_file(name, communities):
     with open(name, "w") as file:
         file.writelines(sequence)
 
+
 def coms_normal(result):
     return [set(x) for x in result]
+
 
 def coms_gen(result):
     return [set(x) for x in next(result)]
 
-def run(dataset, name, algorithm, fn):
-    G = nx.read_edgelist(f"datasets/{dataset}/edges.txt")
-    start = time.time()
-    result = algorithm(G)
-    middle = time.time()
-    communities = fn(result)
-    end = time.time()
-    run_time = round((end - start) * 1000, 6)
-    middle_time = round((middle - start) * 1000, 6)
-    print(f"RTime | {name} | {dataset} | {run_time}", file=open("times.txt", "a"))
-    print(f"MTime | {name} | {dataset} | {middle_time}", file=open("times.txt", "a"))
 
-    write_to_file(f"result-{dataset}-{name}.txt", communities)
-    # Output
-    print(f"Dataset {dataset} completed for algorithm {name}!")
+def run(dataset, name, algorithm, fn):
+    def iteration(i):
+        start = time.time()
+        result = algorithm(G)
+        middle = time.time()
+        communities = fn(result)
+        end = time.time()
+        run_time = round((end - start) * 1000, 6)
+        middle_time = round((middle - start) * 1000, 6)
+        # Time
+        print(f"RTime | {name} | {dataset} | {i} | {run_time}", file=open("times.txt", "a"))
+        print(f"MTime | {name} | {dataset} | {i} | {middle_time}", file=open("times.txt", "a"))
+        write_to_file(f"result-{dataset}-{name}-{i}.txt", communities)
+        # Output
+        print(f"Dataset {dataset} completed iteration {i} for algorithm {name}!")
+
+    G = nx.read_edgelist(f"datasets/{dataset}/edges.txt")
+    iteration(0)
+    if name == "lpa":
+        for i in range(1,10):
+            iteration(i)
     
 
 if __name__ == '__main__':
-    for dataset in ["amazon", "youtube"]:
+    for dataset in ["corporate-small", "corporate"]:
+        run(dataset, "lpa", community.asyn_lpa_communities, coms_normal)
         run(dataset, "gn", community.girvan_newman, coms_gen)
         run(dataset, "gm", community.greedy_modularity_communities, coms_normal)
